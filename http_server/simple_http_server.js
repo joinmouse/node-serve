972d545c1eebcf48f1d122d19d7cfd8e433b5b88
joinmouse <908380434@qq.com>
2017-12-21 23:06:43 +0800

    query

--- http_server/simple_http_server.js
+++ http_server/simple_http_server.js
@@ -2,11 +2,18 @@
 //Schema://host:port/path?query#hash
 
 const http = require('http')
+const qs = require('querystring')
 const server = http.createServer()
 
 server.on('request', (req, res) => {
   const url = req.url
   console.log(url)
+
+  //query分割开
+  const queryString = url.substr(url.indexOf('?')+1, url.length)
+  console.log(queryString)
+  const query = qs.parse(queryString)
+  console.log(query)
 
   let resString
   if (url === '/hello') {
