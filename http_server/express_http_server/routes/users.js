ffb01c2f81a1e0af2644f0f9353a8c42331af4f0
joinmouse <908380434@qq.com>
2017-12-23 12:28:19 +0800

    express

---
+++ http_server/express_http_server/routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+const users = []
+
+/* GET users listing. */
+router.route('/')
+  .get((req, res, next) => {
+    res.json(users)
+  })
+  .post((req, res) => {
+    const user = req.body
+    users.push(user)
+    res.json(user)
+  })
+
+module.exports = router;
